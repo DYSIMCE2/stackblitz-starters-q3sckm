b21b1998957544c5a0e2b9050f7ca1d37848586a
DYSIMCE2 <markosimeonov1@gmail.com>
2024-07-15 13:18:15 +0200

    p2

--- src/main.ts
+++ src/main.ts
@@ -8,36 +8,38 @@ import 'zone.js';
   template: `
   
    
-    <select name="drop down">
+    <select>
       <option>x</option>
       <option>y</option>
       <option>z</option>
     </select>
-    <table name ="tabela" border="1">
-      <tr id="R.0">
+    <table border="1">
+      <div id="R.0">
         <th id="0.0">Tim</th>
-        <th id="0.1">Win</th>
+        <th id="0.1">Win</th> 
         <th id="0.2">Los</th>
-      </tr>
-      <tr id="R1">
+      </div>
+      <div id="R1">
         <td id="1.0">{{ime1}}</td>
         <td id="1.1">72</td>
         <td id="1.2">9</td>
-      </tr>
-      <tr id="R2">
+      </div>
+      <div id="R2">
         <td id="2.0">{{ime2}}</td>
         <td id="2.1">52</td>
         <td id="2.2">28</td>
-      </tr>
-      <tr>
+      </div>
+      <div>
         <td >{{ime3}}</td>
         <td>25</td>
         <td>25</td>
-      </tr>
+      </div>
     </table>
     <table>
       <tr>
-        <button  name="button"  href="youtube.com">{{ime4}}</button>
+        <button>{{ime4}}</button>
+        <button>{{ime5}}</button>
+        <button>{{ime6}}</button>
       </tr>
     </table>
 
@@ -50,7 +52,9 @@ export class App {
   ime1 = 'GSW';
   ime2 = 'LAL';
   ime3 = 'LAC';
-  ime4 = 'youtube';
+  ime4 = 'View';
+  ime5 = 'Edit';
+  ime6 = 'Delete';
 }
 
 bootstrapApplication(App);
