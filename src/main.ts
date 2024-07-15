fdda37d7a4ffcd344dbac8995eabb0a85790cff4
DYSIMCE2 <markosimeonov1@gmail.com>
2024-07-15 13:46:21 +0200

    p5

--- src/main.ts
+++ src/main.ts
@@ -8,43 +8,51 @@ import 'zone.js';
   template: `
   
    
-    <select>
-      <option>x</option>
-      <option>y</option>
-      <option>z</option>
-    </select>
+  
     <table border="1">
       <div id="R.0">
-        <th >Tim</th>
-        <th >Win</th> 
-        <th >Los</th>
-        <button>{{ime4}}</button>
-        <button>{{ime5}}</button>
-        <button>{{ime6}}</button>
+        <th >Team</th>
+        <th >W</th> 
+        <th >L</th>
+        <th> <select>
+        <option><button>{{ime4}}</button></option>
+        <option><button>{{ime5}}</button></option>
+        <option><button>{{ime6}}</button></option>
+      </select></th>
       </div>
       <div id="R1">
         <td >{{ime1}}</td>
         <td >72</td>
         <td >9</td>
-        <button>{{ime4}}</button>
-        <button>{{ime5}}</button>
-        <button>{{ime6}}</button>
+       <td> <select>
+        <option><button>{{ime4}}</button></option>
+        <option><button>{{ime5}}</button></option>
+        <option><button>{{ime6}}</button></option>
+      </select></td>
       </div>
       <div id="R2">
         <td >{{ime2}}</td>
         <td >52</td>
         <td >28</td>
-        <button>{{ime4}}</button>
-        <button>{{ime5}}</button>
-        <button>{{ime6}}</button>
+        <td> <select>
+        <option><button>{{ime4}}</button></option>
+        <option><button>{{ime5}}</button></option>
+        <option><button>{{ime6}}</button></option>
+      </select></td>
       </div>
       <div>
+     
         <td>{{ime3}}</td>
         <td>25</td>
         <td>25</td>
-        <button>{{ime4}}</button>
-        <button>{{ime5}}</button>
-        <button>{{ime6}}</button>
+        <td> <select>
+        <option><button>{{ime4}}</button></option>
+        <option><button>{{ime5}}</button></option>
+        <option><button>{{ime6}}</button></option>
+      </select></td>
+        
+        
+        
       </div>
     </table>
    
