f7322b3a500d70c113afec7c676e4f29a2bfb4ac
DYSIMCE2 <markosimeonov1@gmail.com>
2024-07-15 12:47:41 +0200

    p1

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
-   
+  
    
     <select name="drop down">
       <option>x</option>
@@ -14,30 +14,30 @@ import 'zone.js';
       <option>z</option>
     </select>
     <table name ="tabela" border="1">
-      <tr>
-        <th>Tim</th>
-        <th>Win</th>
-        <th>Los</th>
+      <tr id="R.0">
+        <th id="0.0">Tim</th>
+        <th id="0.1">Win</th>
+        <th id="0.2">Los</th>
+      </tr>
+      <tr id="R1">
+        <td id="1.0">{{ime1}}</td>
+        <td id="1.1">72</td>
+        <td id="1.2">9</td>
+      </tr>
+      <tr id="R2">
+        <td id="2.0">{{ime2}}</td>
+        <td id="2.1">52</td>
+        <td id="2.2">28</td>
       </tr>
       <tr>
-        <td>{{ime1}}</td>
-        <td>72</td>
-        <td>9</td>
-      </tr>
-      <tr>
-        <td>{{ime2}}</td>
-        <td>52</td>
-        <td>28</td>
-      </tr>
-      <tr>
-        <td>{{ime3}}</td>
+        <td >{{ime3}}</td>
         <td>25</td>
         <td>25</td>
       </tr>
     </table>
     <table>
       <tr>
-        <button name="button"  href="youtube.com">{{ime4}}</button>
+        <button  name="button"  href="youtube.com">{{ime4}}</button>
       </tr>
     </table>
 
@@ -46,7 +46,7 @@ import 'zone.js';
   `,
 })
 export class App {
-  name = 'Angular';
+
   ime1 = 'GSW';
   ime2 = 'LAL';
   ime3 = 'LAC';
