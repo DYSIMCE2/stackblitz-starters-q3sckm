395ef98b53d212367b508fad3881c7a1c30697fc
DYSIMCE2 <markosimeonov1@gmail.com>
2024-07-16 13:54:42 +0200

    p2.8

--- src/main.ts
+++ src/main.ts
@@ -6,51 +6,67 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
+  <main> 
+  <header class="th1">
+  <table border="1">
+  <div id="R.0">
+    <th>Team</th>
+    <th>W</th> 
+    <th>L</th>
+    <th> <select>
+    <option><button>{{ime4}}</button></option>
+    <option><button>{{ime5}}</button></option>
+    <option><button>{{ime6}}</button></option>
+  </select></th> 
+  </div>
+  </table>
+  </header>
   
-    <table border="1">
-      <div id="R.0">
-        <th >Team</th>
-        <th >W</th> 
-        <th >L</th>
-        <th> <select>
-        <option><button>{{ime4}}</button></option>
-        <option><button>{{ime5}}</button></option>
-        <option><button>{{ime6}}</button></option>
-      </select></th>
-      </div>
-      <div id="R1">
-        <td >{{ime1}}</td>
-        <td >72</td>
-        <td >9</td>
-       <td> <select>
-        <option><button>{{ime4}}</button></option>
-        <option><button>{{ime5}}</button></option>
-        <option><button>{{ime6}}</button></option>
-      </select></td>
-      </div>
-      <div id="R2">
-        <td >{{ime2}}</td>
-        <td >52</td>
-        <td >28</td>
-        <td> <select>
-        <option><button>{{ime4}}</button></option>
-        <option><button>{{ime5}}</button></option>
-        <option><button>{{ime6}}</button></option>
-      </select></td>
-      </div>
-      <div>
-        <td>{{ime3}}</td>
-        <td>25</td>
-        <td>25</td>
-        <td> <select>
-        <option><button>{{ime4}}</button></option>
-        <option><button>{{ime5}}</button></option>
-        <option><button>{{ime6}}</button></option>
-      </select></td>
+  </main>
+  <table border="1">
+  <div id="R.0">
+    <th>Team</th>
+    <th>W</th> 
+    <th>L</th>
+    <th> <select>
+    <option><button>{{ime4}}</button></option>
+    <option><button>{{ime5}}</button></option>
+    <option><button>{{ime6}}</button></option>
+  </select></th>
+  </div>
+  <div id="R1">
+    <td>{{ime1}}</td>
+    <td>72</td>
+    <td>9</td>
+   <td> <select>
+    <option><button>{{ime4}}</button></option>
+    <option><button>{{ime5}}</button></option>
+    <option><button>{{ime6}}</button></option>
+  </select></td>
+  </div>
+  <div id="R2">
+    <td>{{ime2}}</td>
+    <td>52</td>
+    <td>28</td>
+    <td> <select>
+    <option><button>{{ime4}}</button></option>
+    <option><button>{{ime5}}</button></option>
+    <option><button>{{ime6}}</button></option>
+  </select></td>
+  </div>
+  <div>
+    <td>{{ime3}}</td>
+    <td>25</td>
+    <td>25</td>
+    <td> <select>
+    <option><button>{{ime4}}</button></option>
+    <option><button>{{ime5}}</button></option>
+    <option><button>{{ime6}}</button></option>
+  </select></td>
 
-      </div>
+  </div>
 
-    </table>
+</table>
 
   `,
 })
